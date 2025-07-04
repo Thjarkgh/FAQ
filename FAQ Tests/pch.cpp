fffec8aab9b054601ca37fa7e8ad19460967a91e
Thjarkgh <thjarkgh@heroesofpenta.com>
2025-07-04 17:12:35 -0300

    add IO test helper

--- FAQ Tests/pch.cpp
+++ FAQ Tests/pch.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "pch.h"
+
+IOTestInput::IOTestInput(std::vector<int> chars, std::vector<std::string> lines)
+    : chars(std::move(chars)), lines(std::move(lines)) {
+    chars_index = 0;
+    lines_index = 0;
+}
+
+int IOTestInput::getch() {
+    if (chars_index < chars.size()) {
+        return chars[chars_index++];
+    }
+    throw std::runtime_error("No more characters to read");
+}
+
+std::string IOTestInput::getLine() {
+    if (lines_index < lines.size()) {
+        return lines[lines_index++];
+    }
+    throw std::runtime_error("No more lines to read");
+}
+
+IOTestOutput::IOTestOutput(std::shared_ptr<std::string> output) : output(std::move(output)) {}
+
+void IOTestOutput::write(const std::string& text) {
+    *output += text;
+}
--- FAQ Tests/pch.h
+++ FAQ Tests/pch.h
@@ -5,3 +5,27 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include "program.h"
+
+// Helper to simulate user input and capture output
+class IOTestInput : public ProgramInput {
+private:
+    int chars_index;
+    std::vector<int> chars;
+    int lines_index;
+    std::vector<std::string> lines;
+
+public:
+    IOTestInput(std::vector<int> chars, std::vector<std::string> lines);
+    int getch() override;
+    std::string getLine() override;
+};
+
+class IOTestOutput : public ProgramOutput {
+private:
+    std::shared_ptr<std::string> output;
+
+public:
+    IOTestOutput(std::shared_ptr<std::string> output);
+    void write(const std::string& text) override;
+};
